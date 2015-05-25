94dd933991bf7c1688e2af109a32587c88936fce
namnhse02061 <namnhse02061@fpt.edu.vn>
2015-05-26 00:43:38 +0700

    commit

--- Music/src/main/java/namnhse/MusicApp/ORM/Songs.java
+++ Music/src/main/java/namnhse/MusicApp/ORM/Songs.java
@@ -2,8 +2,6 @@ package namnhse.MusicApp.ORM;
 
 import java.io.Serializable;
 
-
-
 public class Songs implements Serializable {
 	/**
 	 * 
@@ -12,36 +10,40 @@ public class Songs implements Serializable {
 	private int id;
 	private String name;
 	private String link;
-	
-	
+
 	public Songs() {
-		
+
 	}
+
 	public Songs(int id, String name, String link) {
-		
+
 		this.id = id;
 		this.name = name;
 		this.link = link;
 	}
-	
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getLink() {
 		return link;
 	}
+
 	public void setLink(String link) {
 		this.link = link;
 	}
-	
-	
+
 }
